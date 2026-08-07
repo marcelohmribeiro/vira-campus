22e164da8a32e4d55129898fc87473477b2c6ad2
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2026-08-07 16:33:17 -0300

    feat: modelagem do banco

--- back/docker/start.sh
+++ back/docker/start.sh
@@ -3,8 +3,8 @@ set -eu
 echo "Generating Prisma client..."
 npx prisma generate
 
-# echo "Applying Prisma schema..."
-# npx prisma db push
+echo "Applying Prisma schema..."
+npx prisma db push
 
 echo "Starting API..."
 node server.js
