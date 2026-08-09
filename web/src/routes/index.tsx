8e1fd4174aab0fadbb2a09aa925fb7004883676e
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2026-08-09 20:44:15 -0300

    feat: integra os fluxos de anúncios

---
+++ web/src/routes/index.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from 'react-router'
+
+import Cadastro from 'src/pages/(public)/cadastro'
+import Login from 'src/pages/(public)/login'
+import Explorar from 'src/pages/auth/explorar'
+import DetalhesAnuncio from 'src/pages/auth/detalhesAnuncio'
+import EditarAnuncio from 'src/pages/auth/editarAnuncio'
+import AuthLayout from 'src/pages/auth/layout'
+import MeusAnuncios from 'src/pages/auth/meusAnuncios'
+import NovoAnuncio from 'src/pages/auth/novoAnuncio'
+import Index from 'src/pages'
+import Layout from 'src/pages/layout'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{ index: true, element: <Index /> },
+			{ path: 'login', element: <Login /> },
+			{ path: 'cadastro', element: <Cadastro /> },
+			{
+				path: 'auth',
+				element: <AuthLayout />,
+				children: [
+					{ path: 'explorar', element: <Explorar /> },
+					{ path: 'meus-anuncios', element: <MeusAnuncios /> },
+					{ path: 'meus-anuncios/:id/editar', element: <EditarAnuncio /> },
+					{ path: 'anuncios/novo', element: <NovoAnuncio /> },
+					{ path: 'anuncios/:id', element: <DetalhesAnuncio /> },
+					{ path: 'perfil', element: <h1>Perfil</h1> },
+					{ path: 'configuracoes', element: <h1>Configurações</h1> },
+				],
+			},
+		],
+	},
+])
