40606e58d5442bbfb082838f99a4bed497f2a42e
marcelohmribeiro <marcelomarquesribeiro1@gmail.com>
2026-08-09 22:50:03 -0300

    chore: configurando ambiente

--- back/src/services/prisma.js
+++ back/src/services/prisma.js
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/client.js";
 import { settings } from "#src/config";
 
 const connectionString = `${settings.DATABASE_URL}`;
